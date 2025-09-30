466194cafef234425500dc6749b9a4c193ab047a
Johannes Ponge <github@johannes-ponge.de>
2025-09-30 16:27:25 +0200

    updated cumulative_cases calculation

--- src/model_analysis/post_processing/pp_cumulative_cases.jl
+++ src/model_analysis/post_processing/pp_cumulative_cases.jl
@@ -3,20 +3,22 @@ export cumulative_cases
 """
     cumulative_cases(postProcessor::PostProcessor)
 
-Returns a `DataFrame` containing the cumulative infections count of infected individuals in the respective
-disease states exposed, infectious, and removed.
+Calculates the cumulative counts of simulated infections (exposures), recoveries, and deaths over time
+and returns a `DataFrame` containing these cumulative counts for each tick.
+
+If individuals can get reinfected, the cumulative counts will reflect the total number of events,
+including multiple infections, recoveries, and deaths for the same individual.
 
 # Returns
 
 - `DataFrame` with the following columns:
 
-| Name             | Type    | Description                                         |
-| :--------------- | :------ | :-------------------------------------------------- |
-| `tick`           | `Int16` | Simulation tick (time)                              |
-| `exposed_cum`    | `Int64` | Total number of individuals in the exposed state    |
-| `infectious_cum` | `Int64` | Total number of individuals in the infectious state |
-| `removed_cum`    | `Int64` | Total number of individuals in the removed state    |
-
+| Name             | Type    | Description                                                |
+| :--------------- | :------ | :--------------------------------------------------------- |
+| `tick`           | `Int16` | Simulation tick (time)                                     |
+| `exposed_cum`    | `Int64` | Cumulative number of individuals infected up to this tick  |
+| `recovered_cum`  | `Int64` | Cumulative number of individuals recovered up to this tick |
+| `deaths_cum`     | `Int64` | Cumulative number of individuals deceased up to this tick  |
 """
 function cumulative_cases(postProcessor::PostProcessor)::DataFrame
 
@@ -25,26 +27,13 @@ function cumulative_cases(postProcessor::PostProcessor)::DataFrame
         return(load_cache(postProcessor, "cumulative_cases"))
     end
 
-    cases = tick_cases(postProcessor)
-    deaths = tick_deaths(postProcessor)
-
-    res = DataFrame(
-        exposed_cum = zeros(nrow(cases)),
-        infectious_cum = zeros(nrow(cases)),
-        recovered_cum = zeros(nrow(cases)),
-        deaths_cum = zeros(nrow(cases))
-    )
-    
-    for i in 1:nrow(res)
-        res[i, "exposed_cum"] = i == 1 ? cases[1, "exposed_cnt"] : res[i-1, "exposed_cum"] + cases[i, "exposed_cnt"] - cases[i, "infectious_cnt"]
-        res[i, "infectious_cum"] = i == 1 ? cases[1, "infectious_cnt"] : res[i-1, "infectious_cum"] + cases[i, "infectious_cnt"] - cases[i, "removed_cnt"]
-        res[i, "deaths_cum"] = i == 1 ? deaths[1, "death_cnt"] : res[i-1, "deaths_cum"] + deaths[i, "death_cnt"]
-    end
-    res[!, "exposed_cum"] = cumsum(cases[!, "exposed_cnt"])
-    res[!, "infectious_cum"] = cumsum(cases[!, "infectious_cnt"])
-    res[!, "deaths_cum"] = cumsum(deaths[!, "death_cnt"])
-    res[!, "recovered_cum"] = cumsum(cases[!, "removed_cnt"]) .- res[!, "deaths_cum"]
-
+    res = tick_cases(postProcessor) |>
+        df -> DataFrame(
+            tick = df.tick,
+            exposed_cum = cumsum(df.exposed_cnt),
+            recovered_cum = cumsum(df.recovered_cnt),
+            deaths_cum = cumsum(df.dead_cnt)
+        )
 
     # cache dataframe
     store_cache(postProcessor, "cumulative_cases", res)
