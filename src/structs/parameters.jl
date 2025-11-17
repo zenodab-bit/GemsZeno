7cc7236b9ead3ee9026a59a921ee30c1721402ed
Johannes Ponge <github@johannes-ponge.de>
2025-11-17 10:47:31 +0100

    moved AgeGroup struct to dedicated file

--- src/structs/parameters.jl
+++ src/structs/parameters.jl
@@ -8,5 +8,6 @@ abstract type Parameter end
 include("parameters/pathogens.jl")
 include("parameters/vaccines.jl")
 include("parameters/ags.jl")
+include("parameters/age_groups.jl")
 include("parameters/contact_matrix.jl")
 include("parameters/contact_sampling_method_structs.jl")
