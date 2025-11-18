603da733ca99f012d3d71a7cc13ab3e01588214a
Johannes Ponge <github@johannes-ponge.de>
2025-11-18 11:41:37 +0100

    updated logger tests

--- test/loggertest.jl
+++ test/loggertest.jl
@@ -4,13 +4,29 @@
     @testset "InfectionLogger" begin
 
         attributes = [
+            "infection_id",
             "id_a",
             "id_b",
-            "infectious_tick",
-            "removed_tick",
+            "progression_category",
+            "infectiousness_onset",
+            "symptom_onset",
+            "severeness_onset",
+            "hospital_admission",
+            "hospital_discharge",
+            "icu_admission",
+            "icu_discharge",
+            "ventilation_admission",
+            "ventilation_discharge",
+            "severeness_offset",
+            "recovery",
+            "death",
             "tick",
             "setting_id",
-            "setting_type"
+            "setting_type",
+            "lat",
+            "lon",
+            "ags",
+            "source_infection_id",
         ]
 
         @testset "Creation and Basic Functionality" begin
@@ -21,31 +37,35 @@
                 @test length(getproperty(il, Symbol(attr))) == 0
             end
 
-            log!(il,
-                Int32(0), # individual a
-                Int32(0), # individual b
-                Int16(0), # tick
-                Int16(0), # infectious tick
-                Int16(0), # symptoms tick
-                Int16(0), # severeness tick
-                Int16(0), # hospital tick
-                Int16(0), # icu tick
-                Int16(0), # ventilation tick
-                Int16(0), # removed tick
-                Int16(0), # death tick
-                Int8(0), # symptom category
-                Int32(0), # setting id
-                Char(0), # setting type
-                Float32(0), # lat
-                Float32(0), # lon
-                Int32(0), # ags
-                Int32(0) # source infection id
+            log!(
+                logger = il,
+                a = Int32(0),
+                b = Int32(0),
+                progression_category = Symbol(Asymptomatic),
+                tick = Int16(0),
+                infectiousness_onset = Int16(0),
+                symptom_onset = Int16(0),
+                severeness_onset = Int16(0),
+                hospital_admission = Int16(0),
+                hospital_discharge = Int16(0),
+                icu_admission = Int16(0),
+                icu_discharge = Int16(0),
+                ventilation_admission = Int16(0),
+                ventilation_discharge = Int16(0),
+                severeness_offset = Int16(0),
+                recovery = Int16(0),
+                death = Int16(0),
+                setting_id = Int32(0),
+                setting_type = 'h',
+                lat = Float32(0),
+                lon = Float32(0),
+                ags = Int32(0),
+                source_infection_id = Int32(0)
             )
 
+            # check if logged correctly
             for attr in attributes
                 @test length(getproperty(il, Symbol(attr))) == 1
-                # looks weird, but does the job
-                @test getproperty(il, Symbol(attr))[1] == typeof(getproperty(il, Symbol(attr))[1])(0)
             end
 
             # conversion to dataframe
@@ -53,15 +73,22 @@
             @test typeof(df) <: DataFrame
             for attr in attributes
                 @test length(getproperty(df, Symbol(attr))) == 1
-                # looks weird, but does the job
-                @test getproperty(df, Symbol(attr))[1] == typeof(getproperty(df, Symbol(attr))[1])(0)
             end
 
+            # this should not work
+            @test_throws MethodError log!(il, a = Int32(0)) # missing required arguments
+            
         end
 
         @testset "Logging Infections" begin
-            BASE_FOLDER = dirname(dirname(pathof(GEMS)))
-            sim = Simulation(BASE_FOLDER * "/test/testdata/NoInfections.toml")
+            sim = Simulation(pop_size = 1000, infected_fraction = 0.0,
+                pathogen = Pathogen(
+                    name = "TestPathogen",
+                    progressions = [Asymptomatic(
+                        exposure_to_infectiousness_onset = 3,
+                        infectiousness_onset_to_recovery = 7,
+                    )]
+            ))
 
             infecter = (sim|>population|>individuals)[1]
             infectee = (sim|>population|>individuals)[2]
@@ -70,26 +97,64 @@
             il = infectionlogger(sim)
             h = household(infectee, sim)
 
-            infect!(infecter, t, pathogen(sim), sim=sim)
-
+            # infect one agent
+            infect!(infecter, t, pathogen(sim), sim = sim, rng = rng(sim))
+            
             @test il.tick[end] == t
             @test il.id_a[end] == -1
             @test il.id_b[end] == id(infecter)
-            @test il.removed_tick[end] > t
-            @test il.infectious_tick[end] >= t
+            @test il.progression_category[end] == Symbol(Asymptomatic)
+            @test il.infectiousness_onset[end] >= t+3
+            @test il.symptom_onset[end] == GEMS.DEFAULT_TICK
+            @test il.severeness_onset[end] == GEMS.DEFAULT_TICK
+            @test il.hospital_admission[end] == GEMS.DEFAULT_TICK
+            @test il.hospital_discharge[end] == GEMS.DEFAULT_TICK
+            @test il.icu_admission[end] == GEMS.DEFAULT_TICK
+            @test il.icu_discharge[end] == GEMS.DEFAULT_TICK
+            @test il.ventilation_admission[end] == GEMS.DEFAULT_TICK
+            @test il.ventilation_discharge[end] == GEMS.DEFAULT_TICK
+            @test il.severeness_offset[end] == GEMS.DEFAULT_TICK
+            @test il.recovery[end] >= t+10
+            @test il.death[end] == GEMS.DEFAULT_TICK
             @test il.setting_id[end] == GEMS.DEFAULT_SETTING_ID
             @test il.setting_type[end] == '?'
+            @test il.lat[end] === NaN32
+            @test il.lon[end] === NaN32
+            @test il.ags[end] == Int32(-1)
+            @test il.source_infection_id[end] == GEMS.DEFAULT_INFECTION_ID
 
-            t = il.infectious_tick[end]
+            # infect another agent in a household setting
+            t = il.infectiousness_onset[end]
+            infect!(infectee, t, pathogen(sim);
+                sim=sim,
+                rng=rng(sim),
+                infecter_id=id(infecter),
+                setting_id=id(h),
+                setting_type=settingchar(h),
+                source_infection_id = il.infection_id[end])
 
-            infect!(infectee, t, pathogen(sim); sim=sim, infecter_id=id(infecter), setting_id=id(h), setting_type=settingchar(h))
             @test il.tick[end] == t
             @test il.id_a[end] == id(infecter)
             @test il.id_b[end] == id(infectee)
-            @test il.removed_tick[end] > t
-            @test il.infectious_tick[end] >= t
+            @test il.progression_category[end] == Symbol(Asymptomatic)
+            @test il.infectiousness_onset[end] >= t+3
+            @test il.symptom_onset[end] == GEMS.DEFAULT_TICK
+            @test il.severeness_onset[end] == GEMS.DEFAULT_TICK
+            @test il.hospital_admission[end] == GEMS.DEFAULT_TICK
+            @test il.hospital_discharge[end] == GEMS.DEFAULT_TICK
+            @test il.icu_admission[end] == GEMS.DEFAULT_TICK
+            @test il.icu_discharge[end] == GEMS.DEFAULT_TICK
+            @test il.ventilation_admission[end] == GEMS.DEFAULT_TICK
+            @test il.ventilation_discharge[end] == GEMS.DEFAULT_TICK
+            @test il.severeness_offset[end] == GEMS.DEFAULT_TICK
+            @test il.recovery[end] >= t+10
+            @test il.death[end] == GEMS.DEFAULT_TICK
             @test il.setting_id[end] == id(h)
             @test il.setting_type[end] == 'h'
+            @test il.lat[end] === NaN32
+            @test il.lon[end] === NaN32
+            @test il.ags[end] == Int32(-1)
+            @test il.source_infection_id[end] == il.infection_id[end-1]
 
         end
 
@@ -168,44 +233,43 @@
         end
 
         @testset "Logging Deaths" begin
-            # TODO 
-            rs = RandomSampling()
+            # simulation with 100 infected individuals who will all die
+            sim = Simulation(pop_size = 1000, infected_fraction = 0.1,
+                pathogen = Pathogen(
+                    name = "TestPathogen",
+                    progressions = [GEMS.Critical(
+                        exposure_to_infectiousness_onset = 1,
+                        infectiousness_onset_to_symptom_onset = 0,
+                        symptom_onset_to_severeness_onset = 0,
+                        severeness_onset_to_hospital_admission = 0,
+                        hospital_admission_to_icu_admission = 0,
+                        icu_admission_to_icu_discharge = 0,
+                        icu_discharge_to_hospital_discharge = 0,
+                        hospital_discharge_to_severeness_offset = 0,
+                        severeness_offset_to_recovery = 0,
+                        icu_admission_to_death = 0,
+                        death_probability = 1.0
+                    )]
+            ))
 
-            p = Pathogen(id=1, name="Test")
-            exposedtick = Int16(0)
-            indiv = Individual(id=42, sex=1, age=40)
+            # exactly 100 persons should be infected
+            inds = individuals(sim) |>
+                i -> i[is_infected.(i)]
+            n_inf = length(inds)
+            @test n_inf == 100
 
-            indiv.exposed_tick = exposedtick
-            indiv.disease_state = 1
-            indiv.number_of_infections += 1
-            indiv.pathogen_id = id(p)
-
-            Random.seed!(test_rng, 1234)
-            disease_progression!(indiv, p, exposedtick, GEMS.Mild, rng=test_rng)
-
-            # doesn't matter if indiv would have survived, we will kill it nonetheless
-            indiv.death_tick = 1
-
-            h = Household(id=1, individuals=[indiv], contact_sampling_method=rs)
-            stngs = SettingsContainer()
-            add_type!(stngs, Household)
-            add!(stngs, h)
-            sim = Simulation(
-                "",
-                InfectedFraction(0, p),
-                TimesUp(420),
-                Population([indiv]),
-                stngs,
-                "test"
-            )
-
-            for i in individuals(h, sim)
-                update_individual!(i, Int16(1), sim)
-            end
-            @test dead(indiv)
+            # death logger should be empty
             dl = deathlogger(sim)
-            @test dl.id[end] == Int32(42)
-            @test dl.tick[end] == Int16(1)
+            @test length(dl.id) == 0
+
+            # run simulation
+            run!(sim)
+
+            # now everybody should be dead
+            @test sum(is_dead.(inds)) == n_inf 
+
+            # all deaths should have been logged
+            @test length(dl.id) == n_inf
         end
     end
 
