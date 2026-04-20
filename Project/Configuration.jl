c1e8f239021acca59823126c8ec9207ad591bc1c
zenodab <zenodab98@gmail.com>
2026-04-20 17:24:30 +0200

    assign function completed

---
+++ Project/Configuration.jl
@@ -0,0 +1,42 @@
+## Setup
+
+#here we specify the total number of partecipant at the event
+event_size_total = 1000
+
+#here we specify the percentage of seated / standing people
+location_groups_percentage = [0.5, 0.5]
+
+#or we can specify the exact number of people seated/standing
+location_groups_number = [400, 600]
+
+#and say if we want to use the number or the percentage
+location_group_true = true
+
+#here we specify the sex group division as a percentage male / female
+sex_groups_percentage = [0.5 , 0.5]
+
+#here we specify the age group division as a percentage
+age_groups_percentage = [
+0.000,
+0.250,
+0.250,
+0.250,
+0.250,
+0.000,
+0.000,
+0.000
+]
+
+## === Concert Attendance ===
+#code for type of Attendance
+locations_levels = [1, 2]
+no_attendance = -1
+seated  = 1
+standing  = 2
+
+##
+#sex levels
+sex_levels = [1, 2]
+
+
+
