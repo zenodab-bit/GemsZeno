45a08d9e0122e4eedccec1111eb304b8caac1491
zenodab <zenodab98@gmail.com>
2026-05-08 14:24:08 +0200

    Pre version update

--- Project/test.jl
+++
@@ -1,4 +0,0 @@
-# Sum the daily_cases for GlobalSetting ('g')
-global_cases = filter(row -> row.setting_type == 'g', rd_concert.data["dataframes"]["tick_cases_per_setting"])
-total_global_infected = sum(global_cases.daily_cases)
-println("Total infected in GlobalSetting: ", total_global_infected)
