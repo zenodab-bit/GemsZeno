d5930fca8a55738539cf31cc84b2cd95c97772d4
JulianPatzner <Julian.Patzner@uk-halle.de>
2026-05-27 13:40:41 +0200

    optimized on-the-fly population constructor

--- src/methods/rng.jl
+++ src/methods/rng.jl
@@ -19,19 +19,19 @@ function set_global_seed(seed::Int64)
 end
 
 """
-    gems_rand(rng::Xoshiro, args...; enforce_sim_rngs::Bool = ENFORCE_SIM_RNGS)
-    gems_rand(sim::Simulation, args...; enforce_sim_rngs::Bool = ENFORCE_SIM_RNGS)
+    gems_rand(rng::Xoshiro, args...)
+    gems_rand(sim::Simulation, args...)
 
 Reproducibility-safe version of `Random.rand`. Always pass a seeded `Xoshiro` from the simulation object to ensure deterministic results.
-If `enforce_sim_rngs` is set to `true`, an error is thrown when the global RNG is used.
+If the global `ENFORCE_SIM_RNGS` is set to `true`, an error is thrown when the global RNG is used.
 Mainly used for debugging purposes.
 """
-function gems_rand(rng::Xoshiro, args...; enforce_sim_rngs::Bool = ENFORCE_SIM_RNGS)
+@inline function gems_rand(rng::Xoshiro, args...)
     # throw error if global RNG is used and enforcement is enabled
-    enforce_sim_rngs && rng === default_gems_rng() && throw(ArgumentError("Using the global RNG in `gems_rand`."))
+    ENFORCE_SIM_RNGS && rng === default_gems_rng() && throw(ArgumentError("Using the global RNG in `gems_rand`."))
     return Random.rand(rng, args...)
 end
-gems_rand(sim::Simulation, args...; enforce_sim_rngs::Bool = ENFORCE_SIM_RNGS) = gems_rand(rng(sim), args...; enforce_sim_rngs = enforce_sim_rngs)
+@inline gems_rand(sim::Simulation, args...) = gems_rand(rng(sim), args...)
 
 function gems_rand(args...; kwargs...)
     @warn "Calling `gems_rand` without a specific RNG is discouraged. Using the global RNG, which may break simulation reproducibility."
@@ -40,19 +40,19 @@ end
 
 
 """
-    gems_sample(rng::Xoshiro, args...; enforce_sim_rngs::Bool = ENFORCE_SIM_RNGS, kwargs...)
-    gems_sample(sim::Simulation, args...; enforce_sim_rngs::Bool = ENFORCE_SIM_RNGS, kwargs...)
+    gems_sample(rng::Xoshiro, args...; kwargs...)
+    gems_sample(sim::Simulation, args...; kwargs...)
     
 Reproducibility-safe version of `StatsBase.sample`. Always pass a seeded `Xoshiro` from the simulation object to ensure deterministic results.
-If `enforce_sim_rngs` is set to `true`, an error is thrown when the global RNG is used.
+If the global `ENFORCE_SIM_RNGS` is set to `true`, an error is thrown when the global RNG is used.
 Mainly used for debugging purposes.
 """
-function gems_sample(rng::Xoshiro, args...; enforce_sim_rngs::Bool = ENFORCE_SIM_RNGS, kwargs...)
+@inline function gems_sample(rng::Xoshiro, args...; kwargs...)
     # throw error if global RNG is used and enforcement is enabled
-    enforce_sim_rngs && rng === default_gems_rng() && throw(ArgumentError("Using the global RNG in `gems_sample`."))
+    ENFORCE_SIM_RNGS && rng === default_gems_rng() && throw(ArgumentError("Using the global RNG in `gems_sample`."))
     return StatsBase.sample(rng, args...; kwargs...)
 end
-gems_sample(sim::Simulation, args...; enforce_sim_rngs::Bool = ENFORCE_SIM_RNGS, kwargs...) = gems_sample(rng(sim), args...; enforce_sim_rngs = enforce_sim_rngs, kwargs...)
+@inline gems_sample(sim::Simulation, args...; kwargs...) = gems_sample(rng(sim), args...; kwargs...)
 
 function gems_sample(args...; kwargs...)
     @warn "Calling `gems_sample` without a specific RNG is discouraged. Using the global RNG, which may break simulation reproducibility."
@@ -61,19 +61,19 @@ end
 
 
 """
-    gems_sample!(rng::Xoshiro, args...; enforce_sim_rngs::Bool = ENFORCE_SIM_RNGS, kwargs...)
-    gems_sample!(sim::Simulation, args...; enforce_sim_rngs::Bool = ENFORCE_SIM_RNGS, kwargs...)
+    gems_sample!(rng::Xoshiro, args...; kwargs...)
+    gems_sample!(sim::Simulation, args...; kwargs...)
 
 Reproducibility-safe version of `StatsBase.sample!`. Always pass a seeded `Xoshiro` from the simulation object to ensure deterministic results.
-If `enforce_sim_rngs` is set to `true`, an error is thrown when the global RNG is used.
+If the global `ENFORCE_SIM_RNGS` is set to `true`, an error is thrown when the global RNG is used.
 Mainly used for debugging purposes.
 """
-function gems_sample!(rng::Xoshiro, args...; enforce_sim_rngs::Bool = ENFORCE_SIM_RNGS, kwargs...)
+@inline function gems_sample!(rng::Xoshiro, args...; kwargs...)
     # throw error if global RNG is used and enforcement is enabled
-    enforce_sim_rngs && rng === default_gems_rng() && throw(ArgumentError("Using the global RNG in `gems_sample!`."))
+    ENFORCE_SIM_RNGS && rng === default_gems_rng() && throw(ArgumentError("Using the global RNG in `gems_sample!`."))
     return StatsBase.sample!(rng, args...; kwargs...)
 end
-gems_sample!(sim::Simulation, args...; enforce_sim_rngs::Bool = ENFORCE_SIM_RNGS, kwargs...) = gems_sample!(rng(sim), args...; enforce_sim_rngs = enforce_sim_rngs, kwargs...)
+@inline gems_sample!(sim::Simulation, args...; kwargs...) = gems_sample!(rng(sim), args...; kwargs...)
 
 function gems_sample!(args...; kwargs...)
     @warn "Calling `gems_sample!` without a specific RNG is discouraged. Using the global RNG, which may break simulation reproducibility."
@@ -82,19 +82,19 @@ end
 
 
 """
-    gems_shuffle!(rng::Xoshiro, args...; enforce_sim_rngs::Bool = ENFORCE_SIM_RNGS)
-    gems_shuffle!(sim::Simulation, args...; enforce_sim_rngs::Bool = ENFORCE_SIM_RNGS)
+    gems_shuffle!(rng::Xoshiro, args...)
+    gems_shuffle!(sim::Simulation, args...)
 
 Reproducibility-safe version of `Random.shuffle!`. Always pass a seeded `Xoshiro` from the simulation object to ensure deterministic results.
-If `enforce_sim_rngs` is set to `true`, an error is thrown when the global RNG is used.
+If the global `ENFORCE_SIM_RNGS` is set to `true`, an error is thrown when the global RNG is used.
 Mainly used for debugging purposes.
 """
-function gems_shuffle!(rng::Xoshiro, args...; enforce_sim_rngs::Bool = ENFORCE_SIM_RNGS)
+@inline function gems_shuffle!(rng::Xoshiro, args...)
     # throw error if global RNG is used and enforcement is enabled
-    enforce_sim_rngs && rng === default_gems_rng() && throw(ArgumentError("Using the global RNG in `gems_shuffle!`."))
+    ENFORCE_SIM_RNGS && rng === default_gems_rng() && throw(ArgumentError("Using the global RNG in `gems_shuffle!`."))
     return Random.shuffle!(rng, args...)
 end
-gems_shuffle!(sim::Simulation, args...; enforce_sim_rngs::Bool = ENFORCE_SIM_RNGS) = gems_shuffle!(rng(sim), args...; enforce_sim_rngs = enforce_sim_rngs)
+@inline gems_shuffle!(sim::Simulation, args...) = gems_shuffle!(rng(sim), args...)
 
 function gems_shuffle!(args...)
     @warn "Calling `gems_shuffle!` without a specific RNG is discouraged. Using the global RNG, which may break simulation reproducibility."
@@ -103,18 +103,18 @@ end
 
 
 """
-    gems_shuffle(rng::Xoshiro, args...; enforce_sim_rngs::Bool = ENFORCE_SIM_RNGS)
-    gems_shuffle(sim::Simulation, args...; enforce_sim_rngs::Bool = ENFORCE_SIM_RNGS)
+    gems_shuffle(rng::Xoshiro, args...)
+    gems_shuffle(sim::Simulation, args...)
 
 Reproducibility-safe version of `Random.shuffle`. Always pass a seeded `Xoshiro` from the simulation object to ensure deterministic results.
-If `enforce_sim_rngs` is set to `true`, an error is thrown when the global RNG is used.
+If the global `ENFORCE_SIM_RNGS` is set to `true`, an error is thrown when the global RNG is used.
 Mainly used for debugging purposes.
 """
-function gems_shuffle(rng::Xoshiro, args...; enforce_sim_rngs::Bool = ENFORCE_SIM_RNGS)
-    enforce_sim_rngs && rng === default_gems_rng() && throw(ArgumentError("Using the global RNG in `gems_shuffle`."))
+@inline function gems_shuffle(rng::Xoshiro, args...)
+    ENFORCE_SIM_RNGS && rng === default_gems_rng() && throw(ArgumentError("Using the global RNG in `gems_shuffle`."))
     return Random.shuffle(rng, args...)
 end
-gems_shuffle(sim::Simulation, args...; enforce_sim_rngs::Bool = ENFORCE_SIM_RNGS) = gems_shuffle(rng(sim), args...; enforce_sim_rngs = enforce_sim_rngs)
+@inline gems_shuffle(sim::Simulation, args...) = gems_shuffle(rng(sim), args...)
 
 function gems_shuffle(args...)
     @warn "Calling `gems_shuffle` without a specific RNG is discouraged. Using the global RNG, which may break simulation reproducibility."
@@ -123,19 +123,19 @@ end
 
 
 """
-    gems_randn(rng::Xoshiro, args...; enforce_sim_rngs::Bool = ENFORCE_SIM_RNGS)
-    gems_randn(sim::Simulation, args...; enforce_sim_rngs::Bool = ENFORCE_SIM_RNGS)
+    gems_randn(rng::Xoshiro, args...)
+    gems_randn(sim::Simulation, args...)
 
 Reproducibility-safe version of `Random.randn`. Always pass a seeded `Xoshiro` from the simulation object to ensure deterministic results.
-If `enforce_sim_rngs` is set to `true`, an error is thrown when the global RNG is used.
+If the global `ENFORCE_SIM_RNGS` is set to `true`, an error is thrown when the global RNG is used.
 Mainly used for debugging purposes.
 """
-function gems_randn(rng::Xoshiro, args...; enforce_sim_rngs::Bool = ENFORCE_SIM_RNGS)
+@inline function gems_randn(rng::Xoshiro, args...)
     # throw error if global RNG is used and enforcement is enabled
-    enforce_sim_rngs && rng === default_gems_rng() && throw(ArgumentError("Using the global RNG in `gems_randn`."))
+    ENFORCE_SIM_RNGS && rng === default_gems_rng() && throw(ArgumentError("Using the global RNG in `gems_randn`."))
     return Random.randn(rng, args...)
 end
-gems_randn(sim::Simulation, args...; enforce_sim_rngs::Bool = ENFORCE_SIM_RNGS) = gems_randn(rng(sim), args...; enforce_sim_rngs = enforce_sim_rngs)
+@inline gems_randn(sim::Simulation, args...) = gems_randn(rng(sim), args...)
 
 function gems_randn(args...)
     @warn "Calling `gems_randn` without a specific RNG is discouraged. Using the global RNG, which may break simulation reproducibility."
